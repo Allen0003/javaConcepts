cd3a966a3e3352451f9f3123275ae18b3f7cfed3
Allen0003 <apss1943@gmail.com>
2020-11-12 10:54:53 +0800

    Update NULL and garbage collection.java

--- NULL and garbage collection.java
+++ NULL and garbage collection.java
@@ -21,3 +21,46 @@ ArrayList longLivedList = new ArrayList<DataElement>(...);
   
 // set the reference to the irrelevant DataElement to null 
 longLivedList.set(someIndex, null); 
+
+
+
+
+Drawbacks in Reference Counted GC (JAVA)
+  
+Consider two classes A and B having each other’s references.
+class A {
+    private B b;
+
+    public void setB(B b) {
+        this.b = b;
+    }
+}
+
+class B {
+    private A a;
+
+    public void setA(A a) {
+        this.a = a;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        A one = new A();
+        B two = new B();
+
+        // Make the objects refer to each other (creates a circular reference)
+        one.setB(two);
+        two.setA(one);
+
+        // Throw away the references from the main method; the two objects are
+        // still referring to each other
+        one = null;
+        two = null;
+    }
+}
+ 
+
+When we assign null values for the two variables one and two, the external references existed with the class objects (“A” and “B”) created at the beginning will be removed. 
+Still, they won’t be eligible for garbage collection as the reference counters of those two objects will not become zero due to object “A” having its reference inside “B” and the object “B” having its reference inside “A”.
+
